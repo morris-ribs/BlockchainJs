1b3c13261a4c620d78e33dc460926a49bb12c710
morris-ribs <maumribeiro@gmail.com>
2018-10-28 18:30:05 +0100

    Beginning connecting the network

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -1,8 +1,11 @@
 const sha256 = require("sha256");
+const currentNodeUrl = process.argv[3];
 
 function Blockchain() {
   this.chain = [];
   this.pendingTransactions = [];
+  this.currentNodeUrl = currentNodeUrl;
+  this.networkNodes = []; // all nodes in the network
   
   // creating genesis block
    this.createNewBlock(100, "0", "0");
