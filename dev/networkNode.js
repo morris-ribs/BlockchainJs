b781d6a07504c9e1e6f378407e0320f9f74eb578
Mauricio Ribeiro <mauricior@sscmon.silversea.local>
2018-10-30 18:22:55 +0100

    Building Endpoint for registering and broadcasting nodes

--- dev/networkNode.js
+++ dev/networkNode.js
@@ -1,7 +1,9 @@
 const bodyParser = require ("body-parser");
 const express = require ("express");
-const Blockchain = require ("./blockchain");
 const uuid = require("uuid/v1");
+const rp = require("request-promise");
+
+const Blockchain = require ("./blockchain");
 
 const app = express();
 
@@ -49,6 +51,50 @@ app.get("/mine", function (req, res) {
         note: "New block mined successfully",
         block: newBlock
     });
+});
+
+// register and broadcast node
+app.post("/register-and-broadcast-node", function (req, res) {
+    const newNodeUrl = req.body.newNodeUrl;
+    if (bitcoin.networkNodes.indexOf(newNodeUrl) == -1) {
+        bitcoin.networkNodes.push(newNodeUrl);
+    }
+    
+    const regNodesPromises = [];
+    bitcoin.networkNodes.forEach(networkNodeUrl => {
+        const requestOptions = {
+            uri: networkNodeUrl + "/register-node",
+            method: "POST",
+            body: { newNodeUrl: newNodeUrl },
+            json: true
+        };
+        regNodesPromises.push(rp(requestOptions));
+    });
+
+    Promise.all(regNodesPromises)
+    .then(data => {
+        const bulkRegisterOptions = {
+            uri: newNodeUrl + "/register-nodes-bulk",
+            method: "POST",
+            body: { allNetworkNodes: [ ...bitcoin.networkNodes, bitcoin.currentNodeUrl ] },
+            json: true
+        };
+
+        return rp(bulkRegisterOptions);
+    })
+    .then(data => {
+        res.json({ note: "New node registered with network successfully." });
+    });
+});
+
+// register a single node
+app.post("/register-node", function (req, res) {
+    
+});
+
+// register multiple nodes at once
+app.post("/register-nodes-bulk", function (req, res) {
+    
 });
 
 app.listen(port, () => {
