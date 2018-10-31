fd21b679846e26168e27c897e3f8597d12c812c4
morris-ribs <mauricior@sscmon.silversea.local>
2018-10-31 17:10:42 +0100

    Registering nodes in network

--- dev/networkNode.js
+++ dev/networkNode.js
@@ -89,12 +89,26 @@ app.post("/register-and-broadcast-node", function (req, res) {
 
 // register a single node
 app.post("/register-node", function (req, res) {
-    
+    const newNodeUrl = req.body.newNodeUrl;
+    const nodeNotAlreadyPresent = bitcoin.networkNodes.indexOf(newNodeUrl) == -1;
+    const notCurrentNode = bitcoin.currentNodeUrl !== newNodeUrl;
+    if (nodeNotAlreadyPresent && notCurrentNode) {
+        bitcoin.networkNodes.push(newNodeUrl);
+    }
+    res.json({ note: "New node registered successfully." });
 });
 
 // register multiple nodes at once
 app.post("/register-nodes-bulk", function (req, res) {
-    
+    const allNetworkNodes = req.body.allNetworkNodes;
+    allNetworkNodes.forEach(networkNodeUrl => {
+        const nodeNotAlreadyPresent = bitcoin.networkNodes.indexOf(networkNodeUrl) == -1;
+        const notCurrentNode = bitcoin.currentNodeUrl !== networkNodeUrl;
+        if (nodeNotAlreadyPresent && notCurrentNode) {
+            bitcoin.networkNodes.push(networkNodeUrl);
+        }
+    });
+    res.json({ note: "Bulk registration successful." });
 });
 
 app.listen(port, () => {
