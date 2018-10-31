98cffcb835836ccef2ebdc791aaf978d4352e509
morris-ribs <maumribeiro@gmail.com>
2018-10-31 18:24:07 +0100

    Broadcasting mining

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -1,4 +1,5 @@
 const sha256 = require("sha256");
+const uuid = require("uuid/v1");
 const currentNodeUrl = process.argv[3];
 
 function Blockchain() {
@@ -25,29 +26,34 @@ Blockchain.prototype.createNewBlock = function(nonce, previousBlockHash, hash) {
   this.chain.push(newBlock);
 
   return newBlock;
-}
+};
 
 
 Blockchain.prototype.getLastBlock = function() {
   return this.chain[this.chain.length - 1];
-}
+};
 
 Blockchain.prototype.createNewTransaction = function (amount, sender, recipient) {
   const newTransaction = {
     amount: amount,
     sender: sender,
-    recipient: recipient
+    recipient: recipient,
+    transactionId: uuid().split("-").join("")
   };
 
-  this.pendingTransactions.push(newTransaction);
+  return newTransaction;
+};
+
+Blockchain.prototype.addTransactionToPendingTransactions = function (transactionObj) {
+  this.pendingTransactions.push(transactionObj);
   return this.getLastBlock()["index"] + 1;
-}
+};
 
 Blockchain.prototype.hashBlock = function(previousBlockHash, currentBlockData, nonce) {
   const dataAsString = previousBlockHash + nonce.toString() + JSON.stringify(currentBlockData);
   const hash = sha256(dataAsString);
   return hash;
-}
+};
 
 Blockchain.prototype.proofOfWork = function(previousBlockHash, currentBlockData) {
   let nonce = 0;
@@ -61,6 +67,6 @@ Blockchain.prototype.proofOfWork = function(previousBlockHash, currentBlockData)
   }
   // returns the nonce value that creates the correct hash
   return nonce;
-}
+};
 
 module.exports = Blockchain;
--- dev/networkNode.js
+++ dev/networkNode.js
@@ -24,11 +24,35 @@ app.get("/blockchain", function (req, res) {
 });
 
 
-// create a new transaction
+// registers a new transaction into this node
 app.post("/transaction", function (req, res) {
-    const transaction = req.body;
-    const blockIndex = bitcoin.createNewTransaction(transaction.amount, transaction.sender, transaction.recipient);
+    const newTransaction = req.body;
+    const blockIndex = bitcoin.addTransactionToPendingTransactions(newTransaction);
     res.json({ note: `Transaction will be added in block ${blockIndex}.` });
+});
+
+// broadcast a new transaction
+app.post("/transaction/broadcast", function (req, res) {
+    const newTransaction = bitcoin.createNewTransaction(req.body.amount, req.body.sender, req.body.recipient);
+    bitcoin.addTransactionToPendingTransactions(newTransaction);
+    
+    const requestPromises = [];
+
+    // broadcast transaction into the network
+    bitcoin.networkNodes.forEach(networkNodeUrl => {
+        const requestOptions = {
+            uri: networkNodeUrl + "/transaction",
+            method: "POST",
+            body: newTransaction,
+            json: true
+        };
+        requestPromises.push(rp(requestOptions));
+    });
+
+    Promise.all(requestPromises)
+    .then(data => {
+        res.json({ note: "Transaction created and broadcast successfully." });
+    });
 });
 
 // mines a new block
@@ -43,13 +67,40 @@ app.get("/mine", function (req, res) {
     const nonce = bitcoin.proofOfWork(previousBlockHash, currentBlockData);
     const blockHash = bitcoin.hashBlock(previousBlockHash, currentBlockData, nonce);
 
-    // reward for who created the block
-    bitcoin.createNewTransaction(12.5, "00", nodeAddress);
-
     const newBlock = bitcoin.createNewBlock(nonce, previousBlockHash, blockHash);
-    res.json({
-        note: "New block mined successfully",
-        block: newBlock
+    const requestPromises = [];
+    bitcoin.networkNodes.forEach(networkNodeUrl => {
+        const requestOptions = {
+            uri: networkNodeUrl + "/receive-new-block",
+            method: "POST",
+            body: { newBlock: newBlock },
+            json: true
+        };
+
+        requestPromises.push(rp(requestOptions));
+    });
+    
+    Promise.all(requestPromises)
+    .then(data => {
+        // reward for who created the block
+        const requestOptions = {
+            uri: networkNodeUrl + "/transaction/broadcast",
+            method: "POST",
+            body: { 
+                amount: 12.5, 
+                sender: "00", 
+                recipient: nodeAddress 
+            },
+            json: true
+        };
+
+        return rp(requestOptions);
+    })
+    .then(data => {    
+        res.json({
+            note: "New block mined and broadcast successfully",
+            block: newBlock
+        });
     });
 });
 
